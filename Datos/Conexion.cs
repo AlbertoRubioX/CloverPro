72a43973f8606ce9d9f689c0eb83ec5f5a7d68fc
Adrian Heredia <AHeredia@CLOVERTECH.com>
2019-03-29 11:04:12 -0700

    CloverPro 1.1.2.22

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -11,12 +11,12 @@ namespace Datos
     public class Conexion
     {
         
-        public static string cadenaConexion = ConfigurationManager.ConnectionStrings["Cloverprod_ConnectionDebug"].ToString();
+        public static string cadenaConexion = ConfigurationManager.ConnectionStrings["Cloverprod_Connection"].ToString();
         
         private static void Cadena()
         {
             if (string.IsNullOrEmpty(cadenaConexion))
-                cadenaConexion = "Data Source=MEXI1329\\SQLEXPRESS;Initial Catalog=cloverprod;Persist Security Info=True;User ID=sa;Password=saadmin";
+                cadenaConexion = "Data Source=MEXI1328;Initial Catalog=cloverprod;Persist Security Info=True;User ID=sa;Password=saadmin";
         }
         public static string CadenaConexion()
         {
