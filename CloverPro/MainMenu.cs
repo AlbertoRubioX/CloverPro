4f5ce118c99b090d985d237952d143bea5427853
Aaron <gomez.aaron@uabc.edu.mx>
2019-04-04 10:44:01 -0700

    NVersion
    1.1.2.24

--- CloverPro/MainMenu.cs
+++ CloverPro/MainMenu.cs
@@ -19,7 +19,7 @@ namespace CloverPro
     {
         
 
-        private string _lsVersion = "";
+        private string _lsVersion = "1.1.2.24";
         [DllImport("user32.dll")]
         private static extern int GetSystemMetrics(int nIdex);
         private const int SM_TABLETPC = 86;
@@ -33,30 +33,6 @@ namespace CloverPro
         {
             InitializeComponent();
             //Codigo para obtener la version del programa de manera automatica en base a las carpetas generadas por el publish.
-            string path = "T:/wrivera/agonz0/CloverPro/Version/Application Files";
-            string[] array_s_version = Directory.GetDirectories(path);
-
-            Array.Sort(array_s_version);
-            for (int i = 0; i < array_s_version.Length; i++)
-            {
-                string sversion = array_s_version[i];
-
-                DateTime fecha = Directory.GetLastWriteTime(sversion);
-
-                if (fecha > ultimafecha)
-                {
-                    ultimafecha = fecha;
-                    pos = i;
-
-                } 
-            }
-
-            sVersion = array_s_version[pos];
-            sVersion = sVersion.Replace("T:/wrivera/agonz0/CloverPro/Version/Application Files\\CloverPro_", "").Replace("_",".");
-
-            _lsVersion = sVersion;
-
-
         }
 
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
