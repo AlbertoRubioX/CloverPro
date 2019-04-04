5290e15eccc54841292594f4309eb17997f3d116
Aaron <gomez.aaron@uabc.edu.mx>
2019-04-04 13:17:06 -0700

    ConfigDev
    archivos de configuración Dev

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -11,12 +11,12 @@ namespace Datos
     public class Conexion
     {
         
-        public static string cadenaConexion = ConfigurationManager.ConnectionStrings["Cloverprod_Connection"].ToString();
+        public static string cadenaConexion = ConfigurationManager.ConnectionStrings["Cloverprod_ConnectionDebug"].ToString();
         
         private static void Cadena()
         {
             if (string.IsNullOrEmpty(cadenaConexion))
-                cadenaConexion = "mxni3-app-08\mxnilocalapps;Initial Catalog=cloverprod;Persist Security Info=True;User ID=Sa;Password=Admin.10";
+                cadenaConexion = "mxni3-app-08\\MXNILOCALAPPS;Initial Catalog=cloverprod;Persist Security Info=True;User ID=Sa;Password=Admin.10";
         }
         public static string CadenaConexion()
         {
