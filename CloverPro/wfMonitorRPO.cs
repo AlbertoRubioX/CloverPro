abc50d4af470b860e10d0f7d7700ae4ed5449fd8
Adrian Heredia <AHeredia@CLOVERTECH.com>
2019-05-28 07:31:41 -0700

    Revert "Se agregaron filtro de lineas para etiquetas internas para que el poceso sea igual que el de etiquetas"
    This reverts commit bf5de9dad1d3c16e1b62faa8242f99af79e567ea.

--- CloverPro/wfMonitorRPO.cs
+++ CloverPro/wfMonitorRPO.cs
@@ -138,7 +138,6 @@ namespace CloverPro
             Data.Add("M", "Almacén");
             Data.Add("EI", "Etiqueta Interna");// Agregada el dia 13-Marzo-2019
             Data.Add("L", "Tipo Linea");
-            Data.Add("LI", "Tipo Linea Interna");
             Data.Add("A", "Almacenista");
             Data.Add("W", "W.P.");
             Data.Add("T", "Turno Procesado");
@@ -465,10 +464,6 @@ namespace CloverPro
                     }
                     //rpo.Filtro = sFiltro;
                     if (sFiltro == "EI")
-                    {
-                        rpo.ValorFiltro = cbbTipo.SelectedValue.ToString();
-                    }
-                    if (sFiltro == "LI")
                     {
                         rpo.ValorFiltro = cbbTipo.SelectedValue.ToString();
                     }
@@ -1841,18 +1836,7 @@ namespace CloverPro
                 cbbTipo.SelectedIndex = -1;
             }
 
-            if (cbbPrioridad.SelectedValue.ToString() == "LI")
-            {
-                Dictionary<string, string> Data2 = new Dictionary<string, string>();
-                Data2.Add("L", "Largos");
-                Data2.Add("P", "Pony");
-                cbbTipo.DataSource = new BindingSource(Data2, null);
-                cbbTipo.DisplayMember = "Value";
-                cbbTipo.ValueMember = "Key";
-                cbbTipo.SelectedIndex = -1;
-            }
-
-            if (cbbPrioridad.SelectedValue.ToString() == "A")
+            if(cbbPrioridad.SelectedValue.ToString() == "A")
             {
                 OperadorLogica op = new OperadorLogica();
                 op.Planta = cbbPlanta.SelectedValue.ToString();
